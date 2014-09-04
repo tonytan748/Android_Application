e69a3701bfa52e81d41a8ecf63e127fae38ba15c
tan yong <violin748@hotmail.com>
2014-09-04 10:11:43 +0000

    update

--- orkiv/__main__.py
+++ orkiv/__main__.py
@@ -1,11 +1,60 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.boxlayout import AnchorLayout
+from kivy.properties import ObjectProperty
 
-class AccountDetailsForm(BoxLayout):
-	pass
+from sleekxmpp import ClientXMPP
+from kivy.uix.textinput import TextInput
+
+from kivy.uix.modalview import ModalView
+from kivy.uix.label import Label
+
+from sleekxmpp.exceptions import XMPPError
+
+class ConnectionModel(ModelView):
+	def __init__(self,jabber_id,password):
+		super(ConnectionModel,self).__init__(auto_dismiss=False,anchor_y='bottom')
+		self.label=Label(text = 'Connecting to %s...' % jabber_id)
+		self.add_widget(self.label)
+		self.jabber_id = jabber_id
+		self.password = password
+
+class AccountDetailsForm(AnchorLayout):
+	server_box = ObjectProperty()
+	username_box = ObjectProperty()
+	password_box = ObjectProperty()
+
+	def login(self):
+		jabber_id = self.username_box.text + '@' + self.server_box.text
+		model = ConnectionModel(jabber_id,self.password_box.text)
+		model.open()
+
+		password = self.password_box.text
+		app = Orkiv.get_running_app()
+		app.connect_to_jabber(jabber_id,password)
+		print app.xmpp.client_roster.keys()
+		app.xmpp.disconnect()
+class AccountDetailsTextInput(TextInput):
+	next=ObjectProperty()
+	def _keyboard_on_key_down(self,window,keycode,text,modifiers):
+		if keycode[0] == 9:
+			self.next.focus = True
+		elif keycode[0] == 13:
+			self.parent.parent.login()
+		else:
+			super(AccountDetailsTextInput,self)._keyboard_on_key_down(window,keycode,text,modifiers)
 
 class Orkiv(App):
-	pass
+	def connect_to_jabber(self,jabber_id,password):
+		self.xmpp = ClientXMPP(jabber_id,password)
+		self.xmpp.reconnect_max_attempt = 1
+		connected=self.xmpp.connect()
+		if not connected:
+			raise XMPPError('unable to connect')
+
+		self.xmpp.process()
+		self.xmpp.send_presence()
+		self.xmpp.get_roster()
+
 
 Orkiv().run()
 
